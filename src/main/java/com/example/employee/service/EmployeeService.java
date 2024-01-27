0cfb689ae3962f676960f5fbffabed7a8cffd715
ArjunSingh757 <arjunsingh7743@gmail.com>
2024-01-27 13:02:20 +0530

    folder Structure completed

---
+++ src/main/java/com/example/employee/service/EmployeeService.java
@@ -0,0 +1,45 @@
+package com.example.employee.service;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import com.example.employee.entity.Employee;
+import com.example.employee.repository.EmployeeRepo;
+
+@Service
+public class EmployeeService {
+
+	@Autowired
+	private EmployeeRepo repo;
+
+	public void addEmp(Employee e) {
+		repo.save(e);
+	}
+
+	public List<Employee> getAllEmp() {
+		return repo.findAll();
+	}
+
+	public Employee getEMpById(int id) {
+		Optional<Employee> e = repo.findById(id);
+		if (e.isPresent()) {
+			return e.get();
+		}
+		return null;
+	}
+
+	public void deleteEMp(int id) {
+		repo.deleteById(id);
+	}
+
+	public Page<Employee> getEMpByPaginate(int currentPage, int size) {
+		Pageable p = PageRequest.of(currentPage, size);
+		return repo.findAll(p);
+	}
+
+}
